8e7b80ad539d2873104fcf75972b9a65aa16f4a0
qwe <qwe@qwe.com>
2020-11-02 14:13:23 +0000

    xhr test failure

--- 3.Async/1) xhr-get/index.js
+++ 3.Async/1) xhr-get/index.js
@@ -1,5 +1,21 @@
 function xhrGet(url) {
-  // Напиши свой код здесь
+  return new Promise(function (resolve, reject) {
+    const xhr = new XMLHttpRequest()
+    xhr.open('GET', url);
+    xhr.setRequestHeader('content-type', 'application/json')
+    
+    xhr.onload = () => {
+      if (this.status == 201 || this.status == 200) {
+        resolve(this.responseText)
+      }
+      else {
+        let err = this.responseText
+        reject(err)
+      }
+    }
+    
+    xhr.send()
+  })
 }
 
 window.xhrGet = xhrGet;
