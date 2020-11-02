6fd5f727e3bec069d464b4cea72d338de998b9ef
qwe <qwe@qwe.com>
2020-11-02 13:19:29 +0000

    query-object finished

--- 2.Array/2) query-object/index.js
+++ 2.Array/2) query-object/index.js
@@ -1,7 +1,24 @@
 function queryToObject(query) {
-  // Напиши свой код здесь
+  if (query === '' || query === '?') return {}
+  if (query.indexOf('=') === -1) throw new Error("Can't be converted") 
+  let get_query = {}
+  query = query.slice(1)
+  let x = query.split('&')
+  for (let i = 0; i < x.length; i++) {
+    let arr = x[i].split('=')
+    let prop = arr[0]
+    let definition = Number(arr[1]) ? Number(arr[1]) : String(arr[1])
+    definition = definition === 'true' ? true : definition
+    definition = definition === 'false' ? false : definition
+    definition = definition === '0' ? 0 : definition
+    get_query[prop] = definition
+    
+  }
+  return get_query
 };
 
 window.queryToObject = queryToObject;
 
 export default queryToObject;
+
+console.log(queryToObject('?test=123'));
